6ac7a1b40e6110dace9bee223614dd3f84f454e7
prakohad@gmail.com <prakohad@gmail.com>
2018-10-29 20:58:04 +0530

    first commit

---
+++ ROYALENFIELD_SERVICES_WEB/src/main/java/com/app/extremity/main/Test.java
@@ -0,0 +1,24 @@
+package com.app.extremity.main; 
+
+//import org.apache.struts.tiles.actions.TilesAction;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+             
+                 
+@SpringBootApplication(scanBasePackages="com")
+         
+@EntityScan("com.app.extremity.models")
+@EnableJpaRepositories("com.app.extremity.Idao") 
+          
+public class Test {    
+	public static void main(String[] args) {
+		SpringApplication.run(Test.class);
+		//#34588a 
+		System.out.println("started....");
+	}
+} 
